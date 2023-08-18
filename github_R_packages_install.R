e05f9cd8ec2aa1f27b37185c74b3ca5e11a94fc6
Jia Liu <124123291+liujialeo@users.noreply.github.com>
2023-08-18 14:09:33 +0800

    Update github_R_packages_install.R

--- github_R_packages_install.R
+++ github_R_packages_install.R
@@ -14,3 +14,10 @@ devtools::install_github("samuel-marsh/scCustomize")
 ##3.通过gitee.com网站安装
 install.packages("git2r")
 remotes::install_git('https://gitee.com/joyeric_admin_admin/scRNAtoolVis?_from=gitee_search.git')
+
+##4.本地安装已经下载好的zip包
+#如果以上方法都无法安装R包，只能将gihub的zip压缩包下载到本地，进行安装。注意：浏览器下载会带一个_master后缀，最好把它删掉。
+library(devtools)
+devtools::install_local("/path/path2/xxx.zip")
+##或者没有devtools的情况下就直接
+install.packages("/path/path2/xxx.zip",repos=NULL)
